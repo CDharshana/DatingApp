3da1d5d6005ee3fece596e51d6417e05fd92809c
CDharshana <kachirandharshana@gmail.com>
2020-05-02 21:27:24 +0530

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext :  DbContext // .net wala extend karanne : deken
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values{ get; set; }
+       
+    }
+}
